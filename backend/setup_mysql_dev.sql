a0927042b2dbe677745a24bc3e5bf3da96247102
Tommytinny <tommyteenboi@gmail.com>
2025-01-26 00:43:04 +0100

    feat: update the content in mysql setup file

--- backend/setup_mysql_dev.sql
+++ backend/setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- prepares a MySQL server for the project
 
 CREATE DATABASE IF NOT EXISTS DevArena;
-CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY 'Wakeup_11!';
+CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON `DevArena`.* TO 'admin'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'admin'@'localhost';
 FLUSH PRIVILEGES;
